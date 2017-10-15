ca46b69c214a3ef8684fb408c5a5114ebad9d53b
Armen G <gharibi.armen@yahoo.com>
2017-10-14 18:34:43 -0700

    initialing spring mvc with MongoDB

---
+++ src/main/java/com/agharibi/springmvcmongodb/SpringMvcMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.agharibi.springmvcmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcMongodbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/agharibi/springmvcmongodb/SpringMvcMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agharibi.springmvcmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
